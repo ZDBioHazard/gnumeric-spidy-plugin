a6b753bf30d0298462389e3bc01166bb50f071e6
Ryan Turner <zdbiohazard2@gmail.com>
2014-02-20 21:03:53 -0800

    Custom columns Part 2: Make custom columns work.

--- spidy/spidy.py
+++ spidy/spidy.py
@@ -27,6 +27,7 @@ this program. If not, see <http://www.gnu.org/licenses/>
 
 import urllib2
 import json
+import re
 
 
 def spidy_update(gui):
@@ -49,15 +50,28 @@ def spidy_update(gui):
             sheet[col, 0].set_text('=spidy_col("%s", "%s")' % name)
         return  # This is an empty sheet, so there's no point updating it.
 
-    # We'll need this a lot.
-    col = sheet.get_extent().start.col
+    # Find the data columns.
+    cols = {}
+    extent = sheet.get_extent()
+    regex = re.compile(r'^=spidy_col\("(?:.+?)","(.+?)"\)$')
+    for col in range(extent.start.col, extent.end.col + 1):
+        match = regex.match(sheet[col, extent.start.row].get_entered_text())
+        try:  # `match` might be NoneType, so catch AttributeError here too.
+            cols[match.group(1).lower()] = col
+        except (IndexError, AttributeError):
+            pass  # Skip unknown columns so we can have a semi-freeform layout.
+
+    # Make sure there's a 'data_id' column.
+    # TODO: Pop-up some kind of error dialog.
+    if 'data_id' not in cols:
+        print("spidy: No data_id column found.")
+        return
 
     # Iterate through the items and update them.
     # TODO: Spawn some kind of progress dialog, as this can take a while.
-    for row in range(sheet.get_extent().start.row + 1,
-                     sheet.get_extent().end.row + 1):
+    for row in range(extent.start.row + 1, extent.end.row + 1):
         try:  # TODO: ID should be optional. Search by name otherwise.
-            data_id = int(sheet[col, row].get_value())
+            data_id = int(sheet[int(cols['data_id']), row].get_value())
         except (TypeError, ValueError) as error:
             print("spidy: Row %d has no ID: %r" % (row, error))
             continue
@@ -69,10 +83,9 @@ def spidy_update(gui):
             print("spidy: Error fetching %d: [%d] %r" % (row, data_id, error))
             continue
 
-        # TODO: There should be a way to define arbitrary columns.
-        sheet[col + 1, row].set_text(str(data.get('name', "Unknown")))
-        sheet[col + 2, row].set_text(str(data.get('max_offer_unit_price', 0)))
-        sheet[col + 3, row].set_text(str(data.get('min_sale_unit_price', 0)))
+        # Put the loaded data into the cells.
+        for key, col in cols.items():
+            sheet[col, row].set_text(str(data.get(key, "Unknown")))
 
 
 def spidy_col(display, field):
